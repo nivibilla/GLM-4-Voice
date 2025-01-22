8080b02d6a3742ce68e6e11141fa1cda0b928ffa
Srinivas Billa <nivibilla@gmail.com>
2025-01-22 17:03:42 +0530

    Update web_demo.py

--- web_demo.py
+++ web_demo.py
@@ -263,5 +263,7 @@ if __name__ == "__main__":
     # Launch the interface
     demo.launch(
         server_port=args.port,
-        server_name=args.host
+        server_name=args.host,
+        ssr_mode=False,
+        share=True
     )
